4b35370548f2d863f44116893331600d9ff06b92
Qlam77 <qlam3@my.bcit.ca>
2016-12-02 09:02:53 -0800

    Update header.php

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Math Game</title>
+        <title>Math Game 10</title>
         <link rel="stylesheet" href="style/bootstrap.css" type="text/css" />
         <link rel="stylesheet" href="style/styles.css" type="text/css" />
         <meta charset="utf-8" />
